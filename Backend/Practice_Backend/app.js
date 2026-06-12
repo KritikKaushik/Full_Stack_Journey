ad3e319069c2907ef45205bb26b51f01158135ee
Kritik <kritikkaushik2004@gmail.com>
2026-06-12 21:25:30 +0530

    Create app.js

---
+++ Backend/Practice_Backend/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import Userroutes from './routes/User.js';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(Userroutes);
+
+app.post('/api/users/register', );
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
